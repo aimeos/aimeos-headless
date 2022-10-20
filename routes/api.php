7f3079cb5e908dc94a341f4dd3952c00122ef353
Aimeos <aimeos@aimeos.org>
2022-10-20 11:22:37 +0200

    Removed auth prefix from /api/auth/ routes

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group( ['middleware' => 'api', 'prefix' => 'auth'], function( $router ) {
+Route::group( ['middleware' => 'api'], function( $router ) {
     Route::post( 'login', 'App\Http\Controllers\AuthController@login' );
     Route::post( 'logout', 'App\Http\Controllers\AuthController@logout' );
     Route::post( 'refresh', 'App\Http\Controllers\AuthController@refresh' );
