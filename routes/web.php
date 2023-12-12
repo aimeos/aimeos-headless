df46734179945478fedd72cbedc2f32c98d5e9f4
Aimeos <aimeos@aimeos.org>
2023-12-12 12:20:41 +0100

    Removed unnecessary code

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
